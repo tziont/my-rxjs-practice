0518791c7f4b61faf40aac45be1f68cc7a2ec19f
tzion <tzion800@gmail.com>
2022-02-11 23:03:41 +0200

    working with subjects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RxjsOperatorsComponent } from './components/rxjs-operators/rxjs-operators.component';
+import { SubjectComponent } from './components/subject/subject.component';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RxjsOperatorsComponent
+    RxjsOperatorsComponent,
+    SubjectComponent,
   ],
   imports: [
     BrowserModule,
